4a00671449369ec576a65b4ba9266568439063db
alan marinez <alanmartinez319@yahoo.com>
2023-12-20 18:17:16 -0800

    weather app

---
+++ src/api.js
@@ -0,0 +1,19 @@
+// const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+
+
+
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5a613d75eemsh0eb96c33078adc7p1e1bddjsn86cc82b2af71',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const  WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "a8a9045bf0c56117c3ca7762a4dcd3a5"; 
